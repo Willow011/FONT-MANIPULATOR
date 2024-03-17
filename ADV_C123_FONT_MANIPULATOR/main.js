e08279cbf19673d36dba494bf27b56d026e19951
Willow011 <99154745+Willow011@users.noreply.github.com>
2024-03-16 18:32:11 -0700

    Add files via upload

--- ADV_C123_FONT_MANIPULATOR/main.js
+++ ADV_C123_FONT_MANIPULATOR/main.js
@@ -1,3 +1,7 @@
+difference = 0;
+rightWristX = 0;
+leftWristX = 0;
+
 function setup() {
     video = createCapture(VIDEO);
     video.size(550, 500)
@@ -17,6 +21,19 @@ function gotPoses(results)
 {
     if(results.length > 0)
     {
+        console.log(results);
+        leftWristX = results[0].pose.leftWrist.x;
+        rightWristsX = results[0].pose.rightWrist.x;
+        difference = floor(leftWristX - rightWristX);
 
+        console.log("leftWristX = " + leftWristX + " rightWristX = "+ rightWristX + " difference = " + difference);
     }
+}
+
+function draw() {
+    background('#CCCCFF');
+
+    document.getElementById("text_side").innerHTML = "Size of a Text will be = " + difference +"px";
+    fill('#F90093');
+    text(Hello, 10, 10)
 }
